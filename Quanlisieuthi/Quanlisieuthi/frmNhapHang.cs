92b0dd170d17d218412881794617f7afc0f2c1ab
trungkien9996 <31985809+trungkien9996@users.noreply.github.com>
2017-10-26 19:24:51 +0700

    ads

--- Quanlisieuthi/Quanlisieuthi/frmNhapHang.cs
+++ Quanlisieuthi/Quanlisieuthi/frmNhapHang.cs
@@ -19,6 +19,25 @@ namespace Quanlisieuthi
             InitializeComponent();
         }
 
+        private void h()
+        {
+            if (e.RowIndex > -1 && e.ColumnIndex > -1)
+            {
+                txtID.Text = Convert.ToString(dataGridView1.CurrentRow.Cells[0].Value);
+                txtHoTen.Text = Convert.ToString(dataGridView1.CurrentRow.Cells[1].Value);
+                txtQueQuan.Text = Convert.ToString(dataGridView1.CurrentRow.Cells[2].Value);
+                txtGioiTinh.Text = Convert.ToString(dataGridView1.CurrentRow.Cells[3].Value);
+                txtCMND.Text = Convert.ToString(dataGridView1.CurrentRow.Cells[4].Value);
+                DateTime dt;
+                dt = Convert.ToDateTime(dataGridView1.CurrentRow.Cells[5].Value);
+                txtNgaySinh.Text = dt.ToShortDateString();
+            }
+            else
+            {
+                return;
+            }
+        }
+
         private void frmNhapHang_Load(object sender, EventArgs e)
         {
             txtNN.Text = DateTime.Now.ToShortDateString();
