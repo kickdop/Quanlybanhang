1fa2cd8a71a528f7efb0d36293593cead728c162
hitekvn <31841044+hitekvn@users.noreply.github.com>
2017-10-26 11:29:28 +0700

    c

--- Quanlisieuthi/Quanlisieuthi/HoaDon.cs
+++ Quanlisieuthi/Quanlisieuthi/HoaDon.cs
@@ -17,6 +17,31 @@ namespace Quanlisieuthi
         {
             InitializeComponent();
         }
-       
+        ConnectData conn = new ConnectData();
+        public string constr = @"select * from dbo.HoaDon";
+
+        private void but_Up_Click(object sender, EventArgs e)
+        {
+            conn.MoKetNoi();
+            SqlCommand sqlcm = new SqlCommand("Edit_HoaDon", conn.conn);
+            sqlcm.CommandType = CommandType.StoredProcedure;
+            sqlcm.Parameters.AddWithValue("@id_hoadon", txtID.Text);
+            sqlcm.Parameters.AddWithValue("@ten_hoadon", txtHoaDon.Text);
+            sqlcm.Parameters.AddWithValue("@id_hanghoa", txtHangHoa.Text);
+            sqlcm.Parameters.AddWithValue("@id_nhanvien", txtNhanVien.Text);
+            sqlcm.Parameters.AddWithValue("@id_khachhang", txtKhachHang.Text);
+            int check = sqlcm.ExecuteNonQuery();
+            if (check > 0)
+            {
+                MessageBox.Show("Sửa thành công");
+                conn.KhoiTao(dataGridView1, @"select * from HoaDon");
+                txtID.Text = txtHoaDon.Text = txtKhachHang.Text = txtHangHoa.Text = txtNhanVien.Text = String.Empty;
+            }
+            else
+            {
+                MessageBox.Show("Có lỗi");
+            }
+            conn.DongKetNoi();
+        }
     }
 }
