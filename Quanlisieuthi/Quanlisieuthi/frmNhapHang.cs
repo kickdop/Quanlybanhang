eedb914e531de201a4e450c97d122baa46ec0b89
trungkien9996 <31985809+trungkien9996@users.noreply.github.com>
2017-10-29 12:12:59 +0700

    sad

--- Quanlisieuthi/Quanlisieuthi/frmNhapHang.cs
+++ Quanlisieuthi/Quanlisieuthi/frmNhapHang.cs
@@ -19,25 +19,6 @@ namespace Quanlisieuthi
             InitializeComponent();
         }
 
-        private void h()
-        {
-            if (e.RowIndex > -1 && e.ColumnIndex > -1)
-            {
-                txtID.Text = Convert.ToString(dataGridView1.CurrentRow.Cells[0].Value);
-                txtHoTen.Text = Convert.ToString(dataGridView1.CurrentRow.Cells[1].Value);
-                txtQueQuan.Text = Convert.ToString(dataGridView1.CurrentRow.Cells[2].Value);
-                txtGioiTinh.Text = Convert.ToString(dataGridView1.CurrentRow.Cells[3].Value);
-                txtCMND.Text = Convert.ToString(dataGridView1.CurrentRow.Cells[4].Value);
-                DateTime dt;
-                dt = Convert.ToDateTime(dataGridView1.CurrentRow.Cells[5].Value);
-                txtNgaySinh.Text = dt.ToShortDateString();
-            }
-            else
-            {
-                return;
-            }
-        }
-
         private void frmNhapHang_Load(object sender, EventArgs e)
         {
             txtNN.Text = DateTime.Now.ToShortDateString();
@@ -46,7 +27,24 @@ namespace Quanlisieuthi
 
         private void butNhapHang_Click(object sender, EventArgs e)
         {
-            
+            con.MoKetNoi();
+            SqlCommand sqlcm = new SqlCommand("NhapHang", con.conn);
+            sqlcm.CommandType = CommandType.StoredProcedure;
+            sqlcm.Parameters.AddWithValue("@tenhang", txtTenHang.Text);
+            sqlcm.Parameters.AddWithValue("@giaban", int.Parse(txtGB.Text));
+            sqlcm.Parameters.AddWithValue("@soluong", int.Parse(txtSL.Text));
+            sqlcm.Parameters.AddWithValue("@ngaynhap", Convert.ToDateTime(txtNN.Text).ToShortDateString());
+            int check = (int)sqlcm.ExecuteNonQuery();
+            if (check > 0)
+            {
+                MessageBox.Show("Nhập thành công");
+                txtGB.Text = txtNN.Text = txtTenHang.Text = txtSL.Text = string.Empty;
+            }
+            else
+            {
+                MessageBox.Show("Có lỗi");
+            }
+            con.DongKetNoi();
         }
     }
 }
