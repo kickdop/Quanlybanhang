b8eae608344d92ca7e5a099ba2bd90f238c02231
hitekvn <31841044+hitekvn@users.noreply.github.com>
2017-10-30 15:36:11 +0700

    c

--- Quanlisieuthi/Quanlisieuthi/KhachHang.cs
+++ Quanlisieuthi/Quanlisieuthi/KhachHang.cs
@@ -17,6 +17,38 @@ namespace Quanlisieuthi
         {
             InitializeComponent();
         }
-       
+        ConnectData conn = new ConnectData();
+        public string constr = @"select * from dbo.KhachHang";
+
+        private void but_Ins_Click(object sender, EventArgs e)
+        {
+            but_OK.Visible = true;
+            but_Ins.Visible = false;
+            txtTen.Text = txtSDT.Text = txtGT.Text = txtID.Text = "";
+            dataGridView1.Enabled = false;
+        }
+
+        private void but_Up_Click(object sender, EventArgs e)
+        {
+            conn.MoKetNoi();
+            SqlCommand sqlcm = new SqlCommand("Edit_KhachHang", conn.conn);
+            sqlcm.CommandType = CommandType.StoredProcedure;
+            sqlcm.Parameters.AddWithValue("@id_khachhang", txtID.Text);
+            sqlcm.Parameters.AddWithValue("@ten_khachhang", txtTen.Text);
+            sqlcm.Parameters.AddWithValue("@SDT_khachhang", txtSDT.Text);
+            sqlcm.Parameters.AddWithValue("@gioitinh_khachhang", txtGT.Text);
+            int check = sqlcm.ExecuteNonQuery();
+            if (check > 0)
+            {
+                MessageBox.Show("Sửa thành công");
+                conn.KhoiTao(dataGridView1, @"select * from KhachHang");
+                txtID.Text = txtTen.Text = txtSDT.Text = txtGT.Text = String.Empty;
+            }
+            else
+            {
+                MessageBox.Show("Có lỗi");
+            }
+            conn.DongKetNoi();
+        }
     }
 }
